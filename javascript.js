ea91fe101b2c1f49292f71e0defaf8ce9c301ead
Sagar22829 <raoprem43@gmail.com>
2025-03-17 16:47:40 +0530

    Delete javascript.js

--- javascript.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Your message has been sent! We will get back to you soon.');
-});
